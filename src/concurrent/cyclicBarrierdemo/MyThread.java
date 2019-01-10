dcaa50a7977590195d3c76b575113060767be035
黄智浩 <buhui.hzh@dtyunxi.com>
2019-01-10 10:40:30 +0800

    新建信号量学习

--- src/concurrent/cyclicBarrierdemo/MyThread.java
+++ src/concurrent/cyclicBarrierdemo/MyThread.java
@@ -1,6 +1,7 @@
 package concurrent.cyclicBarrierdemo;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 public class MyThread extends Thread {
 
@@ -13,7 +14,7 @@ public class MyThread extends Thread {
     public void run() {
         System.out.print(Thread.currentThread().getName() + " going to await" + "\n");
         try {
-            cb.await();
+            cb.await(5, TimeUnit.SECONDS);
             System.out.print(Thread.currentThread().getName() + " continue" + "\n");
         } catch (Exception e) {
             e.printStackTrace();
